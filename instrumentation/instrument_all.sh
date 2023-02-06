ad4c0504f93bc4b824f177132d5d27123a54d0df
LC Yu <liangcheng.yu46@gmail.com>
2023-02-06 03:18:22 +0000

    Add instrument_all.sh

---
+++ instrumentation/instrument_all.sh
@@ -0,0 +1,7 @@
+./instrument.sh -t hw -r $HOME/FP4/test_programs/p4_14/Firewall/hardware_rules.txt $HOME/FP4/test_programs/p4_14/Firewall/Firewall.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/LoadBalance/hardware_rules.txt $HOME/FP4/test_programs/p4_14/LoadBalance/LoadBalance.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/RateLimiter/hardware_rules.txt $HOME/FP4/test_programs/p4_14/RateLimiter/RateLimiter.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/basic_routing/hardware_rules.txt $HOME/FP4/test_programs/p4_14/basic_routing/basic_routing.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/dv_router/hardware_rules.txt $HOME/FP4/test_programs/p4_14/dv_router/dv_router.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/mirror_clone/hardware_rules.txt $HOME/FP4/test_programs/p4_14/mirror_clone/mirror_clone.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/netchain/hardware_rules.txt $HOME/FP4/test_programs/p4_14/netchain/netchain.p4
