f0f9ead2ace4a2fb09726a7d7692ba0352f46500
LC Yu <liangcheng.yu46@gmail.com>
2023-02-11 01:52:15 +0000

    Add script to combine ut/dt p4

---
+++ hardware_run/compile_all_dt_hw.sh
@@ -0,0 +1,8 @@
+sudo -E ./compile_p4_14.sh Firewall_dt_hw.p4
+sudo -E ./compile_p4_14.sh LoadBalance_dt_hw.p4
+sudo -E ./compile_p4_14.sh RateLimiter_dt_hw.p4
+sudo -E ./compile_p4_14.sh basic_routing_dt_hw.p4
+sudo -E ./compile_p4_14.sh dv_router_dt_hw.p4
+sudo -E ./compile_p4_14.sh mirror_clone_dt_hw.p4
+sudo -E ./compile_p4_14.sh netchain_dt_hw.p4
+
---
+++ hardware_run/compile_all_ut_hw.sh
@@ -0,0 +1,8 @@
+sudo -E ./compile_p4_14.sh Firewall_ut_hw.p4
+sudo -E ./compile_p4_14.sh LoadBalance_ut_hw.p4
+sudo -E ./compile_p4_14.sh RateLimiter_ut_hw.p4
+sudo -E ./compile_p4_14.sh basic_routing_ut_hw.p4
+sudo -E ./compile_p4_14.sh dv_router_ut_hw.p4
+sudo -E ./compile_p4_14.sh mirror_clone_ut_hw.p4
+sudo -E ./compile_p4_14.sh netchain_ut_hw.p4
+
