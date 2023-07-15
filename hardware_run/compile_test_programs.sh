e70e32f9dfdf1c21f43b3b4cb18ed6e033610524
LC Yu <liangcheng.yu46@gmail.com>
2023-07-15 18:13:31 +0000

    Clean up the script

--- hardware_run/compile_test_programs.sh
+++ hardware_run/compile_test_programs.sh
@@ -9,54 +9,25 @@ set -ex
 
 start=$(date +%s)
 
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/Firewall
-cd $HOME/FP4/test_programs/p4_14/Firewall
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh Firewall.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
+program_list=(
+   "firewall"
+   "load_balance"
+   "rate_limiter"
+   "basic_routing"
+   "dv_router"
+   "mirror_clone"
+   "netchain"
+)
 
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/LoadBalance
-cd $HOME/FP4/test_programs/p4_14/LoadBalance
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh LoadBalance.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
-
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/RateLimiter
-cd $HOME/FP4/test_programs/p4_14/RateLimiter
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh RateLimiter.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
-
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/basic_routing
-cd $HOME/FP4/test_programs/p4_14/basic_routing
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh basic_routing.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
-
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/dv_router
-cd $HOME/FP4/test_programs/p4_14/dv_router
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh dv_router.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
-
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/mirror_clone
-cd $HOME/FP4/test_programs/p4_14/mirror_clone
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh mirror_clone.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
-
-cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/netchain
-cd $HOME/FP4/test_programs/p4_14/netchain
-rm *.c || echo "*.c not found"
-sudo -E ./compile_p4_14.sh netchain.p4
-rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
-cd -
+for program in "${program_list[@]}"
+do
+    cp compile_p4_14.sh $HOME/FP4/test_programs/p4_14/$program
+    cd $HOME/FP4/test_programs/p4_14/$program
+    rm *.c || echo "*.c not found"
+    sudo -E ./compile_p4_14.sh $program.p4
+    rm compile_p4_14.sh || echo "compile_p4_14.sh not found"
+    cd -
+done
 
 end=$(date +%s)
 difference=$(( end - start ))
