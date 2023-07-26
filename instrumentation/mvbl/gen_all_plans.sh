48b973d90841556b003bc09016273afe4aac31e4
LC Yu <liangcheng.yu46@gmail.com>
2023-07-26 06:15:59 +0000

    Focus on getting switch.p4 instrumentation plan

--- instrumentation/mvbl/gen_all_plans.sh
+++ instrumentation/mvbl/gen_all_plans.sh
@@ -5,30 +5,32 @@ set -ex
 SDE_PREFIX=/home/leoyu/bf-sde-9.2.0/pkgsrc/p4-build/tofino
 
 program_list=(
-   "basic_routing"
-   # "firewall"
+   # "basic_routing"
+   # "mirror_clone"
    # "load_balance"
    # "rate_limiter"
-   # "basic_routing_old"
-   # "dv_router"
-   # "mirror_clone"
    # "netchain"
+   # "firewall"
+   # "dv_router"
+   "switch_80019"
 )
 
 context_dot_cache_dir="context_dot_cache"
 
-rm -rf $context_dot_cache_dir
-mkdir $context_dot_cache_dir
+mkdir $context_dot_cache_dir || echo "Skipped"
 
 for program in "${program_list[@]}"
 do
-    mkdir $context_dot_cache_dir/$program
-    cp $SDE_PREFIX/$program/graphs/ingress.dot $context_dot_cache_dir/$program
-    cp $SDE_PREFIX/$program/graphs/egress.dot $context_dot_cache_dir/$program
-    cp $SDE_PREFIX/$program/context.json $context_dot_cache_dir/$program
-    dot -Tsvg $context_dot_cache_dir/$program/ingress.dot > $context_dot_cache_dir/$program/ingress.svg
-    dot -Tsvg $context_dot_cache_dir/$program/egress.dot > $context_dot_cache_dir/$program/egress.svg
-    python3 gen_mvbl_plan.py -d $SDE_PREFIX/$program/graphs/ingress.dot -c $SDE_PREFIX/$program/context.json
+   rm -rf $context_dot_cache_dir/$program
+   mkdir $context_dot_cache_dir/$program
+   cp $SDE_PREFIX/$program/graphs/ingress.dot $context_dot_cache_dir/$program
+   cp $SDE_PREFIX/$program/graphs/egress.dot $context_dot_cache_dir/$program
+   cp $SDE_PREFIX/$program/context.json $context_dot_cache_dir/$program
+   cp $SDE_PREFIX/$program/logs/mau.json $context_dot_cache_dir/$program
+   dot -Tsvg $context_dot_cache_dir/$program/ingress.dot > $context_dot_cache_dir/$program/ingress.svg
+   dot -Tsvg $context_dot_cache_dir/$program/egress.dot > $context_dot_cache_dir/$program/egress.svg
+   python3 gen_mvbl_plan.py -p $program -d $SDE_PREFIX/$program/graphs/ingress.dot -c $SDE_PREFIX/$program/context.json -g ingress
+   python3 gen_mvbl_plan.py -p $program -d $SDE_PREFIX/$program/graphs/egress.dot -c $SDE_PREFIX/$program/context.json -g egress
 done
 
 # python3 gen_mvbl_plan.py -d test_examples/switch_80019/ingress.dot -c test_examples/switch_80019/context.json
