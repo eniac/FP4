109fafb0b8c75ab92bdabd5b87ba5b0b31c65df1
LC Yu <liangcheng.yu46@gmail.com>
2023-07-29 22:17:44 +0000

    Update program path

--- instrumentation/instrument_all_hw.sh
+++ instrumentation/instrument_all_hw.sh
@@ -3,9 +3,11 @@ rm -rf out
 mkdir sample_out
 mkdir out
 
-./instrument.sh -t hw -r $HOME/FP4/test_programs/p4_14/Firewall/hardware_rules.txt $HOME/FP4/test_programs/p4_14/Firewall/Firewall.p4
-./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/LoadBalance/hardware_rules.txt $HOME/FP4/test_programs/p4_14/LoadBalance/LoadBalance.p4
-./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/RateLimiter/hardware_rules.txt $HOME/FP4/test_programs/p4_14/RateLimiter/RateLimiter.p4
+set -ex
+
+./instrument.sh -vv -t hw -r $HOME/FP4/test_programs/p4_14/firewall/hardware_rules.txt $HOME/FP4/test_programs/p4_14/firewall/firewall.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/load_balance/hardware_rules.txt $HOME/FP4/test_programs/p4_14/load_balance/load_balance.p4
+./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/rate_limiter/hardware_rules.txt $HOME/FP4/test_programs/p4_14/rate_limiter/rate_limiter.p4
 ./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/basic_routing/hardware_rules.txt $HOME/FP4/test_programs/p4_14/basic_routing/basic_routing.p4
 ./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/dv_router/hardware_rules.txt $HOME/FP4/test_programs/p4_14/dv_router/dv_router.p4
 ./instrument.sh -n -t hw -r $HOME/FP4/test_programs/p4_14/mirror_clone/hardware_rules.txt $HOME/FP4/test_programs/p4_14/mirror_clone/mirror_clone.p4
