5d4b2200a58191f7f7ae0619d9e55f6b3ab8cd21
LC Yu <liangcheng.yu46@gmail.com>
2023-08-01 04:51:10 +0000

    Get resource usage of UT

--- hardware_run/compile_all_ut_hw.sh
+++ hardware_run/compile_all_ut_hw.sh
@@ -1,8 +1,11 @@
-sudo -E ./compile_p4_14.sh Firewall_ut_hw.p4
-sudo -E ./compile_p4_14.sh LoadBalance_ut_hw.p4
-sudo -E ./compile_p4_14.sh RateLimiter_ut_hw.p4
+#! /bin/bash
+
+set -ex
+
+sudo -E ./compile_p4_14.sh firewall_ut_hw.p4
+sudo -E ./compile_p4_14.sh load_balance_ut_hw.p4
+sudo -E ./compile_p4_14.sh rate_limiter_ut_hw.p4
 sudo -E ./compile_p4_14.sh basic_routing_ut_hw.p4
 sudo -E ./compile_p4_14.sh dv_router_ut_hw.p4
 sudo -E ./compile_p4_14.sh mirror_clone_ut_hw.p4
 sudo -E ./compile_p4_14.sh netchain_ut_hw.p4
-
