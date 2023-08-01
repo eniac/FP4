a368b0b725ca9b760e57952e1f72e257a782ba59
LC Yu <liangcheng.yu46@gmail.com>
2023-08-01 06:07:19 +0000

    Fix BUG with 0 return of make

--- hardware_run/compile_all_ut_hw.sh
+++ hardware_run/compile_all_ut_hw.sh
@@ -1,11 +1,17 @@
-#! /bin/bash
-
+#!/bin/bash
 set -ex
 
-sudo -E ./compile_p4_14.sh ut_hw/firewall_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/load_balance_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/rate_limiter_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/basic_routing_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/dv_router_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/mirror_clone_ut_hw.p4
-sudo -E ./compile_p4_14.sh ut_hw/netchain_ut_hw.p4
+files=("ut_hw/firewall_ut_hw.p4" 
+       "ut_hw/load_balance_ut_hw.p4" 
+       "ut_hw/rate_limiter_ut_hw.p4" 
+       "ut_hw/basic_routing_ut_hw.p4" 
+       "ut_hw/dv_router_ut_hw.p4" 
+       "ut_hw/mirror_clone_ut_hw.p4" 
+       "ut_hw/netchain_ut_hw.p4")
+
+for file in "${files[@]}"; do
+    if sudo -E ./compile_p4_14.sh "$file" 2>&1 | tee /dev/stderr | grep -q "error:"; then
+        echo "Compilation failed: $file"
+        exit 1
+    fi
+done
