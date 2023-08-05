6aad45072b5a8da20897c30b7a8fc6dbf0831d85
LC Yu <liangcheng.yu46@gmail.com>
2023-08-05 22:32:29 +0000

    Update compile_all_dt_hw.sh

--- hardware_run/compile_all_dt_hw.sh
+++ hardware_run/compile_all_dt_hw.sh
@@ -1,8 +1,17 @@
-sudo -E ./compile_p4_14.sh Firewall_dt_hw.p4
-sudo -E ./compile_p4_14.sh LoadBalance_dt_hw.p4
-sudo -E ./compile_p4_14.sh RateLimiter_dt_hw.p4
-sudo -E ./compile_p4_14.sh basic_routing_dt_hw.p4
-sudo -E ./compile_p4_14.sh dv_router_dt_hw.p4
-sudo -E ./compile_p4_14.sh mirror_clone_dt_hw.p4
-sudo -E ./compile_p4_14.sh netchain_dt_hw.p4
+#!/bin/bash
+set -ex
 
+files=("dt_hw/firewall_dt_hw.p4" 
+       "dt_hw/load_balance_dt_hw.p4" 
+       "dt_hw/rate_limiter_dt_hw.p4" 
+       "dt_hw/basic_routing_dt_hw.p4" 
+       "dt_hw/dv_router_dt_hw.p4" 
+       "dt_hw/mirror_clone_dt_hw.p4" 
+       "dt_hw/netchain_dt_hw.p4")
+
+for file in "${files[@]}"; do
+    if sudo -E ./compile_p4_14.sh "$file" 2>&1 | tee /dev/stderr | grep -q "error:"; then
+        echo "Compilation failed: $file"
+        exit 1
+    fi
+done
