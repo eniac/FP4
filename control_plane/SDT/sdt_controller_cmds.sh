b12317dd82eb3ee1da5d71cb3906e7d389883c4b
LC Yu <liangcheng.yu46@gmail.com>
2023-08-06 19:35:02 +0000

    Minor

--- control_plane/SDT/sdt_controller_cmds.sh
+++ control_plane/SDT/sdt_controller_cmds.sh
@@ -1,7 +1,7 @@
 sudo -E python -u SDTcontroller.py -p basic_routing_dt_hw -r basic_routing_ut_hw_rules.txt | tee log_basic_routing.txt
 sudo -E python -u SDTcontroller.py -p dv_router_dt_hw -r dv_router_ut_hw_rules.txt | tee log_dv_router.txt
-sudo -E python -u SDTcontroller.py -p Firewall_dt_hw -r Firewall_ut_hw_rules.txt | tee log_Firewall.txt
-sudo -E python -u SDTcontroller.py -p LoadBalance_dt_hw -r LoadBalance_ut_hw_rules.txt | tee log_LoadBalance.txt
+sudo -E python -u SDTcontroller.py -p firewall_dt_hw -r firewall_ut_hw_rules.txt | tee log_firewall.txt
+sudo -E python -u SDTcontroller.py -p load_balance_dt_hw -r load_balance_ut_hw_rules.txt | tee log_load_balance.txt
 sudo -E python -u SDTcontroller.py -p mirror_clone_dt_hw -r mirror_clone_ut_hw_rules.txt | tee log_mirror_clone.txt
 sudo -E python -u SDTcontroller.py -p netchain_dt_hw -r netchain_ut_hw_rules.txt | tee log_netchain.txt
-sudo -E python -u SDTcontroller.py -p RateLimiter_dt_hw -r RateLimiter_ut_hw_rules.txt | tee log_RateLimiter.txt
+sudo -E python -u SDTcontroller.py -p rate_limiter_dt_hw -r rate_limiter_ut_hw_rules.txt | tee log_rate_limiter.txt
