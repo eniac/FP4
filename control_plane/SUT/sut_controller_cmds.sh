4454ac1290d39223c083768ec513edba2e3f7714
LC Yu <liangcheng.yu46@gmail.com>
2023-08-09 04:34:33 +0000

    Fix typo in mac learning rules

--- control_plane/SUT/sut_controller_cmds.sh
+++ control_plane/SUT/sut_controller_cmds.sh
@@ -6,3 +6,4 @@ python SUTcontroller.py -p mirror_clone_ut_hw
 python SUTcontroller.py -p netchain_ut_hw
 python SUTcontroller.py -p rate_limiter_ut_hw
 sudo -E python SUTcontroller.py -p dv_router_ut_hw -d dv_router_controller | tee log_sut_dv_router.txt
+sudo -E python SUTcontroller.py -p mac_learning_ut_hw -d mac_learning_controller | tee log_sut_mac_learning.txt
