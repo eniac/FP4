5147c0297f251a34568ebae29e636af6386f3e43
LC Yu <liangcheng.yu46@gmail.com>
2023-08-09 20:18:24 +0000

    Minor

--- control_plane/SUT/dos_defense_controller.py
+++ control_plane/SUT/dos_defense_controller.py
@@ -19,7 +19,6 @@ def run_dynamic(static_controller, *args, **kwargs):
 
     installed_rules = []
 
-    # TODO: data plane not mirroring any packets, probably the mirror bug, add 1 rule to bootstrap; however, still not receiving any rules...
     ruleList = []
     # ruleList.append("pd ipv4_lpm add_entry ai_handle_blacklist_pfuzz_ipv4_lpm ipv4_dstAddr 50.0.0.1 ipv4_dstAddr_prefix_length 8 ")
     static_controller.generate_output_rules(ruleList)
@@ -44,6 +43,9 @@ def run_dynamic(static_controller, *args, **kwargs):
                 static_controller.add_entries()
                 installed_rules.extend(ruleList)
 
+    # Debug read registers
+
+
 class Controller(pystate.TrackState):
     @pystate.track_init
     def __init__(self, _dp_socket, visited_bytes=8):
@@ -51,11 +53,10 @@ class Controller(pystate.TrackState):
         self.routingTable = defaultdict(lambda: {"cost": INFINITY, "nhAddr":''})
         self.existingEntries = []
         self.dvLock = threading.Lock()
-        self.myIPs = []
         self.visited_bytes = visited_bytes
-        self.visited_bytes = 10  # TODO: change hardcoding
+        self.visited_bytes = 12  # TODO: change hardcoding
 
-        self.src_macs = set()
+        self.dst_ips = set()
         self.SDTsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     @pystate.track_stack_calls
@@ -64,25 +65,23 @@ class Controller(pystate.TrackState):
         sys.stdout.flush()
         ruleList = []
         fp4_header = packet[:self.visited_bytes]
-        index = self.visited_bytes + 6
-        # TODO: read the source port from temp_port in fp4_header
-        src_mac = struct.unpack("!BBBBBB", packet[index:index+6])
-        src_mac = Controller.prettify_mac(src_mac)
-        print("--- src_mac: {} ---".format(src_mac))
+        index = self.visited_bytes + 14  # Ethernet
+        index = self.visited_bytes + 16  # ipv4 and dst_ip
+        dst_ip = Controller.prettify_ip(packet[index:index+4])
+        print("--- dst_ip: {} ---".format(dst_ip))
         sys.stdout.flush()
-        # if src_mac not in self.src_macs:
-        #     self.src_macs.add(src_mac)
-        #     # Install rules for tiLearnMAC
-        #     rule = ("pd tiLearnMAC add_entry aiNoOp_pfuzz_tiLearnMAC ethernet_srcAddr 0x" + str(src_mac).replace(":",""))
-        #     ruleList.append(rule)
 
-        #     # Instal rules for tiForward
-        #     port_list = [i for i in range(0, 4, 60)]
-        #     random_port = random.choice(port_list)
-        #     rule = ("pd tiForward add_entry aiForward_pfuzz_tiForward ethernet_dstAddr 0x" + str(src_mac).replace(":","") + " action_egress_port " + str(random_port))
-        #     ruleList.append(rule)
+        if dst_ip not in self.dst_ips:
+            self.dst_ips.add(dst_ip)
+            ruleList.append("pd ipv4_lpm add_entry ai_handle_blacklist_pfuzz_ipv4_lpm ipv4_dstAddr " + dst_ip + " ipv4_dstAddr_prefix_length 8")
+
         return ruleList
 
     @staticmethod
     def prettify_mac(mac_string):
         return ':'.join('%02x' % b for b in mac_string)
+
+    @staticmethod
+    def prettify_ip(ip_bytes):
+        ip_bytes = struct.unpack("!BBBB", ip_bytes)
+        return '.'.join([str(b) for b in ip_bytes])
